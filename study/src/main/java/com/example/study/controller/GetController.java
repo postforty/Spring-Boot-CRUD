627b5bcfc2e0ecf0bd17d8d1e3072131e53c6494
postforty <75767073+postforty@users.noreply.github.com>
2022-08-11 21:09:15 +0900

    [ADD] getParameter

--- study/src/main/java/com/example/study/controller/GetController.java
+++ study/src/main/java/com/example/study/controller/GetController.java
@@ -1,7 +1,9 @@
 package com.example.study.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,14 @@ public class GetController {
 	public String getRequest() {
 		
 		return "Hi getMethod";
+	}
+	
+	@GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+	public String getParameter(@RequestParam String id, @RequestParam String password) {
+		System.out.println("id : "+id);
+		System.out.println("password : "+password);
+		
+		return id+password;
+		
 	}
 }
