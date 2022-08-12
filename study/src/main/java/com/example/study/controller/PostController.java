295a057dee4dd47a2a8a7044d027cc7ac2c39cf6
postforty <75767073+postforty@users.noreply.github.com>
2022-08-12 17:14:53 +0900

    [ADD] PUT, Patch Method

--- study/src/main/java/com/example/study/controller/PostController.java
+++ study/src/main/java/com/example/study/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.study.controller;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,19 @@ public class PostController {
 	// ajax 검색
 	// http post body -> data
 	// json, xml, multipart-form / text-plain
-	
+	// 객체를 json으로 리턴한다.
 	@PostMapping("/postMethod")
 	public SearchParam postMethod(@RequestBody SearchParam searchParam) {
 		return searchParam;
+	}
+	
+	@PutMapping("/putMethod")
+	public void put() {
+		
+	}
+	
+	@PatchMapping("/patchMethod")
+	public void patch() {
+		
 	}
 }
