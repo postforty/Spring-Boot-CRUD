e31d494b7d50f83527743329706a4ce2ecb5fb8c
postforty <75767073+postforty@users.noreply.github.com>
2022-08-13 11:51:40 +0900

    [ADD] JPA - GET

--- study/src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ study/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.study.repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +15,29 @@ public class UserRepositoryTest extends StudyApplicationTests {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@Test
-	public void create() {
-		User user = new User();
-		user.setAccount("TestUser03");
-		user.setEmail("TestUser03@gmail.com");
-		user.setPhoneNumber("010-3333-3333");
-		user.setCreatedAt(LocalDateTime.now());
-		user.setCreatedBy("TestUser03");
-		
-		User newUser = userRepository.save(user);
-		System.out.println("newUser : "+newUser);
-				
-	}
+//	@Test
+//	public void create() {
+//		User user = new User();
+//		user.setAccount("TestUser03");
+//		user.setEmail("TestUser03@gmail.com");
+//		user.setPhoneNumber("010-3333-3333");
+//		user.setCreatedAt(LocalDateTime.now());
+//		user.setCreatedBy("TestUser03");
+//		
+//		User newUser = userRepository.save(user);
+//		System.out.println("newUser : "+newUser);
+//				
+//	}
 	
+	@Test
 	public void read() {
+		Optional<User> user = userRepository.findById(2L);
+		
+		user.ifPresent(selectUser ->{
+			System.out.println("user : "+selectUser);
+			System.out.println("email : "+selectUser.getEmail());
+			
+		});
 		
 	}
 	
